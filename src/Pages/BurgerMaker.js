99ef985c0f9b62f50c5233badfd87e73dfdd35f9
sekararumk <sekararumk21@gmail.com>
2022-03-29 23:20:43 +0700

    first commit

---
+++ src/Pages/BurgerMaker.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+import Burger from '../Components/Burger/Burger'
+
+const BurgerMaker = () => {
+    return (
+        <Burger />
+    )
+}
+
+export default BurgerMaker
